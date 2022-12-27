0e0f7ba7189169a6cb45dc6c71a3ce76a2ac081c
luiizsilverio <luiiz.silverio@gmail.com>
2022-12-27 19:45:45 -0300

    27/12/22

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,25 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limpaFormulario();
+    }
+    criaNegociacao() {
+        const date = new Date(this.inputData.value.replace('-', ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    limpaFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
